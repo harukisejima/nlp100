abc1376441d6083e7cff76aeec86cfdf3fb2cd69
瀬島大生 <sejimadaisei@haruki.local>
2019-05-09 11:36:05 +0900

    nineteen finish

---
+++ chapter2/nlp19.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import collections
+file = 'hightemp.txt'
+with open(file) as open_file:
+    lines = open_file.readlines()
+    # 県だけを取り出してkensに格納する
+    kens = [line.split('\t')[0]for line in lines]
+    # Counterを使って頻出頻度を調べる
+    count = collections.Counter(kens)
+    # count.most_commo()がタプルで（要素:頻出頻度）で返して来るので一つずつ取り出す
+    for w,c in count.most_common():
+        # 表示
+        print(w,c)
+
