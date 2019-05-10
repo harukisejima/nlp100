5113a3882a5de0ea1b53a5510e330db0ec3a0c54
瀬島大生 <sejimadaisei@haruki.local>
2019-05-10 14:07:03 +0900

    twenty-second finish

---
+++ chapter3/nlp22.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import gzip # https://docs.python.org/ja/3/library/gzip.html ファイルの圧縮、展開インターフェース
+import json #jsonを扱うためのライブラリ
+import re #正規表現を扱うライブラリ
+
+def chose_text():  
+    filename  = "jawiki-country.json.gz"
+    #reading modeでファイルを開く
+    with gzip.open(filename,'rt') as open_file:
+        #1行ずつjsonファイルを読んで行く
+        for text in open_file:
+            #json文字列を辞書に変換
+            jsondata = json.loads(text)
+            #titleがイギリスだったら
+            if jsondata['title'] == "イギリス":
+                #本文を表示
+                return jsondata['text']
+
+# 正規表現のコンパイル
+#[[Category:と]]で括られている
+#(?:)キャプチャしない文字列
+pattern = re.compile(r'^\[\[Category:(.*?)(?:\|.*)?\]\]$', re.MULTILINE + re.VERBOSE)
+# 抽出
+result = pattern.findall(chose_text())
+
+# 結果表示
+for line in result:
+    print(line)
